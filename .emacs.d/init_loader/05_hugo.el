db676586dbe37965938f95448d040522e85074e4
ryuichit <ryuichi.tanimoto@gmail.com>
2021-04-27 18:07:55 +0900

    add easy-hugo

---
+++ .emacs.d/init_loader/05_hugo.el
@@ -0,0 +1,7 @@
+;; ----------------------------------------
+;; easy-hugo
+;; ----------------------------------------
+(define-key global-map (kbd "C-c C-e") 'easy-hugo)
+;; テックブログ用設定
+(setq easy-hugo-basedir "~/stockmark/git/techblog/")
+(setq easy-hugo-postdir "content/blog")
