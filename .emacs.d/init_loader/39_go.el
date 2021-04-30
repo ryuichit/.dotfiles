2b5052293fe761fac4561a0de56457c7e9b4334a
ryuichit <ryuichi.tanimoto@gmail.com>
2021-05-01 00:36:35 +0900

    modify company mode color

--- .emacs.d/init_loader/39_go.el
+++ .emacs.d/init_loader/39_go.el
@@ -8,6 +8,24 @@
                           ))
 
 ;; ----------------------------------------
+;; company-mode
+;; ----------------------------------------
+(set-face-attribute 'company-tooltip nil
+		            :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common nil
+		            :foreground "black" :background "lightgrey")
+(set-face-attribute 'company-tooltip-common-selection nil
+		            :foreground "white" :background "steelblue")
+(set-face-attribute 'company-tooltip-selection nil
+		            :foreground "black" :background "steelblue")
+(set-face-attribute 'company-preview-common nil
+		            :background nil :foreground "lightgrey" :underline t)
+(set-face-attribute 'company-scrollbar-fg nil
+		            :background "grey60")
+(set-face-attribute 'company-scrollbar-bg nil
+		            :background "gray40")
+
+;; ----------------------------------------
 ;; lsp-mode
 ;; ----------------------------------------
 ;; go-modeのときlspする
