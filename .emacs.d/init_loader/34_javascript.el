0e68a0ed50f3d9f7a5ff060675b35a58cf2e9403
ryuichit <ryuichi.tanimoto@gmail.com>
2022-07-29 13:50:04 +0900

    use js2-mode on mjs files

--- .emacs.d/init_loader/34_javascript.el
+++ .emacs.d/init_loader/34_javascript.el
@@ -3,3 +3,9 @@
           (lambda ()
             (make-local-variable 'js-indent-level)
             (setq js-indent-level 2)))
+
+;; ----------------------------------------
+;; js2-mode
+;; ----------------------------------------
+;; mjsではjs2-modeを使う。
+(add-to-list 'auto-mode-alist '("\\.mjs\\'" . js2-mode))
