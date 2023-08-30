238a5124bb1f70d8423a681c87a01af98b2094a8
ryuichit <ryuichi.tanimoto@gmail.com>
2023-08-30 08:40:57 +0000

    configure_zsh.sh: support some linux distributions

--- configure_zsh.sh
+++ configure_zsh.sh
@@ -1,4 +1,20 @@
-if [ "$SHELL" == "/bin/bash" ]; then
-    sudo apt install zsh
+if [ "$SHELL" = "/bin/bash" ]; then
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        case $ID in
+            ubuntu)
+                sudo apt install zsh
+                ;;
+            centos)
+                sudo yum install zsh
+                ;;
+            amzn)
+                sudo yum install zsh
+                ;;
+            *)
+                echo "Unknown Linux OS"
+                ;;
+        esac
+    fi
     chsh -s $(which zsh)
 fi
